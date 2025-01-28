9fe8ef34395c836b04a1441f93d0faca2338fa2b
Elias Rojas <rojas.elias@outlook.com>
2025-01-27 22:53:12 -0300

    creating stuff for styles

--- portal/wikiapp/models.py
+++ portal/wikiapp/models.py
@@ -1,3 +1,24 @@
+from typing import final
 from django.db import models
 
 # Create your models here.
+
+@final
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey("Menu", on_delete=models.CASCADE)
+
+@final
+class File(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    references = models.ManyToManyField("File")
+    extension = models.CharField(max_length=12)
+    last_time_modified = models.DateField()
+
+
+@final
+class Folder(models.Model):
+    parent = models.ForeignKey("Folder", on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
