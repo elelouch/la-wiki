d52745676a1bfe9890dbf51de7e8ea4b4cf04d0e
Elias Rojas <rojas.elias@outlook.com>
2025-02-03 13:05:13 +0000

    sape

--- portal/core/models.py
+++ portal/core/models.py
@@ -1,54 +1,52 @@
 # pyright: reportUnknownVariableType=false
 from django.db import models
 from typing import final
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User
 
 @final
-class Directory(models.Model):
+class AccessList(models.Model):
+    can_read = models.BooleanField(default=False)
+    can_write = models.BooleanField(default=False)
+    user = models.ForeignKey(
+            User,
+            null=True,
+            on_delete=models.CASCADE,
+            related_name="dir_access_list"
+        )
+
+@final
+class PermissionHandler(models.Model):
+    access_list = models.ManyToManyField(AccessList)
+
+    def user_has_perm(self, user, perm):
+        access_list = AccessList.objects.get(user_id=user.id)
+        if perm == 'read':
+            return access_list.can_read
+        if perm == 'write':
+            return access_list.can_write
+        return False
+
+@final
+class Section(models.Model):
     name = models.CharField(max_length=256, default="")
     description = models.CharField(max_length=256, default="")
     parent = models.ForeignKey(
             "self",
             on_delete=models.CASCADE,
             null=True,
-            related_name="directories"
+            related_name="sections"
             )
-
-# can_read_directory
-# can_write_directory
-
+        
 @final
-class DirectoryAccessList(models.Model):
-    user = models.ForeignKey(
-            User,
-            null=True,
-            on_delete=models.CASCADE,
-            related_name="dir_access_list"
-            )
-    permissions = models.ManyToManyField(Permission)
-    dir = models.ForeignKey(Directory, null=True, on_delete=models.CASCADE)
-
-@final
-class File(models.Model):
+class Archive(models.Model):
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
-    references = models.ManyToManyField("File")
+    references = models.ManyToManyField("self")
     extension = models.CharField(max_length=12)
     last_time_modified = models.DateField()
-    folder = models.ForeignKey(
-            Directory, 
+    section = models.ForeignKey(
+            Section, 
             on_delete=models.CASCADE,
             null=True,
-            related_name="files"
+            related_name="archives"
             )
-
-@final
-class FileAccessList(models.Model):
-    user = models.ForeignKey(
-            User,
-            null=True,
-            on_delete=models.CASCADE,
-            related_name="file_access_list"
-            )
-    permissions = models.ManyToManyField(Permission)
-    dir = models.ForeignKey(File, null=True, on_delete=models.CASCADE)
