da8ce4d063eea9b7676df383680fa5a4f69468d8
Elias Rojas <rojas.elias@outlook.com>
2025-02-05 16:22:34 +0000

    adding modal forms

--- portal/core/urls.py
+++ portal/core/urls.py
@@ -5,5 +5,7 @@ app_name = "core"
 
 urlpatterns = [
         path("", views.WikiView.as_view(), name="home"),
-        path("section/<int:section_id>/", views.DirectoryView.as_view(), name="section"),
+        path("section/<int:section_id>/", views.SectionView.as_view(), name="section"),
+        path("modal_section", views.FileModalView.as_view(), name="section"),
+        path("modal_file", views.SectionModalView.as_view(), name="section"),
         ]
--- portal/core/views.py
+++ portal/core/views.py
@@ -6,22 +6,20 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import mixins
 from django.db.models import Q
 from django.urls import reverse_lazy
-from core.forms import SectionForm
+from core.forms import SectionForm, FileForm
 from typing import override
 
 from .models import Section, Archive
 
-class DirectoryView (mixins.LoginRequiredMixin, TemplateView):
+class SectionView (mixins.LoginRequiredMixin, TemplateView):
     template_name = "core/section_view.html"
     login_url = reverse_lazy("wikiapp:login")
     redirect_field_name="login"
 
     @override
     def get(self, request, section_id):
-
         assert self.template_name is not None
         root_section = None
-
         user = request.user
         if not section_id:
             root_section = user.main_section
@@ -29,26 +27,37 @@ class DirectoryView (mixins.LoginRequiredMixin, TemplateView):
                 return HttpResponseNotFound("Main section not available")
         else :
             root_section = get_object_or_404(Section, pk=section_id)
-
         sections = root_section.sections \
                 .filter(
                         Q(access_lists__groups__user__id = user.id) | Q(access_lists__user__id = user.id),
                         access_lists__can_read = True)
-
         return render(request, self.template_name, {"sections": sections})
 
     def post(self, request, section_id):
         section = get_object_or_404(Section, pk=section_id)
         user = request.user
-
         if section.user_has_perm(section, "write"):
             form = request.POST
-            print(form)
-
         return render(request, self.template_name)
+
+class SectionModalView(mixins.LoginRequiredMixin, TemplateView):
+    template_name = "core/section_modal_form.html"   
+    extra_context = {"section_modal_form": SectionForm()}
+
+    def post(self, request): 
+        # gotta redirect view
+        return render(self, self.template_name)
+
+class FileModalView(mixins.LoginRequiredMixin, TemplateView):
+    template_name = "core/file_modal_form.html"   
+    extra_context = {"file_modal_form": FileForm()}
+
+    def post(self, request): 
+        # gotta redirect view
+        return render(self, self.template_name)
+
 
 class WikiView (mixins.LoginRequiredMixin, TemplateView):
     template_name = "core/main.html"
     login_url = reverse_lazy("wikiapp:login")
     redirect_field_name="login"
-    extra_context = {"section_form": SectionForm()}
