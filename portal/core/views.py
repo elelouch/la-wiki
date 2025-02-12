91fe57440c7e7f74f7922ae4706f6404eb926b9e
Elias Rojas <rojas.elias@outlook.com>
2025-02-12 16:01:00 +0000

    no perdamos tiempo

--- portal/core/views.py
+++ portal/core/views.py
@@ -21,28 +21,20 @@ class ChildrenView (mixins.LoginRequiredMixin, TemplateView):
     def get(self, request: HttpRequest, root_section_id: int, mode:str):
         assert self.template_name is not None
         user = request.user
-
         if mode not in [perm.value for perm in PermissionType]:
             return HttpResponse("mode not valid, must used read or write", status=400)
-
         if not root_section_id:
             return HttpResponse("Root section must be an integer", status=400)
-
         root_section = get_object_or_404(Section, pk=root_section_id)
-
         if not root_section.user_has_perm(user, PermissionType.READ):
             return HttpResponse("User does not have read permission", status = 403)
-
         sections = root_section.children \
                 .filter(
                         Q(access_lists__group__user = user) | Q(access_lists__user = user),
                         access_lists__can_read = True)
-
         if mode == PermissionType.WRITE.value:
             sections = sections.filter(access_lists__can_write=True)
-
         archives = root_section.archives.all()
-
         context = {
                 "root_section": root_section,
                 "sections": sections,
@@ -60,43 +52,29 @@ class ModalSectionView(mixins.LoginRequiredMixin, TemplateView):
 
     def post(self, request: HttpRequest, root_section_id: int): 
         root_section = get_object_or_404(Section, pk=root_section_id)
-
         user = request.user
         user_can_write = root_section.user_has_perm(user, PermissionType.WRITE)
-
         if not user_can_write:
             return HttpResponse("Unauthorized", status=401)
-
         new_sec = root_section.children.create(name = request.POST["name"])
-
         # inherits permissions
         new_sec.access_lists.add(*root_section.access_lists.all())
-
-        return HttpResponse("success", 
-                            headers={
-                                "HX-Trigger": "new_section"
-                                },
-                            status = 200)
-
+        return HttpResponse(
+            "success", 
+            headers={"HX-Trigger": "new_section," + "new_section_parent_" + str(root_section_id)},
+            status = 200
+        )
+    
 
 class SectionView(mixins.LoginRequiredMixin, TemplateView):
     def delete(self, request, root_section_id): 
         root_section = get_object_or_404(Section, pk=root_section_id)
-
         user = request.user
         user_can_write = root_section.user_has_perm(user, PermissionType.WRITE)
-
         if not user_can_write:
             return HttpResponse("Unauthorized", status=401)
-
-        parent_id = root_section.parent.id
         root_section.delete()
-
-        return HttpResponse("success", 
-                            headers={
-                                "HX-Trigger": "deleted_section"
-                                },
-                            status = 200)
+        return HttpResponse("success", headers={"HX-Trigger": "deleted_section"},status = 200)
 
 # get/post for appending a file to a section
 @final
@@ -104,23 +82,12 @@ class ModalFileView(mixins.LoginRequiredMixin, TemplateView):
     template_name = "core/file_modal_form.html"   
     extra_context = {"form": FileForm()}
 
-    def post(self, request, root_section_id): 
-        return render(self, self.template_name)
-
-    @override
-    def dispatch(self, *args, **kwargs):
-        response = super(GetParticularUserView, self).dispatch(*args, **kwargs)
-        response['HX-Trigger'] = "modal_form"
-        return response
-
-
 @final
 class WikiViewRead (mixins.LoginRequiredMixin, TemplateView):
     template_name = "core/main.html"
     login_url = reverse_lazy("wikiapp:login")
     extra_context = {"mode": PermissionType.READ.value}
     redirect_field_name="login"
-
 
 @final
 class WikiViewWrite (mixins.LoginRequiredMixin, TemplateView):
